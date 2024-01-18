938f837449b19699b1494fa6d5e9421d0c34a41d
Kwame Carr <kwame_91@hotmail.com>
2024-01-18 18:33:49 +0100

    removed react project

--- kwamsc/src/components/Layout/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as Grid} from './Grid';
-export {default as FullWidthSection} from './FullWidthSection';
-export {default as MaxWidthSection} from './MaxWidthSection';
-export {default as Row} from './Row';
--- kwamsc/src/styles/theme.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  breakpoints: {
-    s: "only screen and (max-width: 50rem)",
-    m: "only screen and (max-width: 60rem)",
-  },
-};
